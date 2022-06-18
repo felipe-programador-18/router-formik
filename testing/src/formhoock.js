61cca7cb593ca2f1a1e1d27f33b2f176a6bc7020
Felipe <machadofelipe2016@outlook.com>
2022-06-18 12:14:12 -0300

    c5-create all structure to practice about other form with hoock

---
+++ testing/src/formhoock.js
@@ -0,0 +1,78 @@
+import React from "react"
+import * as yup from 'yup'
+import { useFormik } from "formik"
+
+const uf =['RS','SP', 'SC','RJ']
+
+const schema = yup.object().shape({
+    name: yup.string().required("Field fill Please"),
+    email: yup.string().required("Field fill Please").email('This not is email')
+})
+
+// make form another way with react hook form!!
+const StructurForm = () => {
+  //create manage process with hook useformik!!
+  const form = useFormik({
+    initialValues:{
+        name:'',
+        email:'',
+        uf:'',
+        subscribe: false
+
+    },
+    onSubmit: async(values) => {
+     console.log(values)
+    },
+    
+   validationSchema: schema
+  })
+   
+  const getAllform = () => {
+    console.log(form)
+  }
+
+
+
+
+
+
+
+  return(<>
+        <h1>Practice little bit about form hook</h1>
+
+        <form  onSubmit={form.handleSubmit} >
+            
+            <label> Name:
+                <input name='name' type='email' value={form.values.name}  onChange={form.handleChange} onBlur={form.handleBlur} />
+            </label>
+           
+           <label> Email:
+                <input name='email' type='email'  value={form.values.email} onChange={form.handleChange} onBlur={form.handleBlur} />
+           </label>
+                  
+
+            <select name='uf'  value={form.values.uf} onChange={form.handleChange}  onBlur={form.handleBlur} >
+                 {uf.map(uf => <option key={uf} value={uf} > {uf} </option>)}
+            </select>
+
+            <input type='checkbox' name='subscribe' value={form.values.subscribe} onChange={form.handleChange} onBlur={form.handleBlur} /> 
+
+             <button onClick={getAllform} >Get form!</button>
+
+
+             <pre> {JSON.stringify(form.values, null, 2)} </pre>
+
+
+        </form>
+  
+  
+  
+  
+  </>
+  )
+}
+
+
+
+
+export default StructurForm
