9ff28055f8be0f40b66e536b71a53b99f3257b25
Felipe <machadofelipe2016@outlook.com>
2022-06-24 17:54:13 -0300

    c20-adding new code

--- testing/src/component/Navbar.js
+++ testing/src/component/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavNav = () => {
   return(<>
-  
-  <h1>This is my Navbar!!!</h1>
+  <nav> 
+    <h1>Home here</h1>
+
+    <Link to={'/'} >Home</Link>
+    <Link to={'/testing'} >Testing</Link>
+  </nav>
   
   </>)
 }
