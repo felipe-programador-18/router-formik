1cb583b4e581be93cbc9018231e6571400a227c4
Felipe <machadofelipe2016@outlook.com>
2022-07-06 18:33:30 -0300

    c49-adding some modify about code

--- testing/src/component/Search.js
+++ testing/src/component/Search.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from  "./searchmodule.css"
+import { useNavigate } from 'react-router-dom'
 
 
-const Search = () => {
-    
+ export const Search = () => {
+     // remind with useNavigate
+     const navigate = useNavigate()
+
+     const [query, setQuery] = useState()
+     const handlSumit = (e) => {
+        e.preventDefault();
+        navigate("/search?q=" + query)
+      }
+
+
     return ( <div className={styles.container} >
          
           <h2>Search results date!!</h2>
-         <form  >
+         <form onSubmit={handlSumit}  >
             <label >
-                <input type='search' name='search'/>
+                <input type='text' onClick={(e) => setQuery(e.target.value) } />
             </label>
+            <label>
+                <input type='submit'  value='Buscar' />
+            </label>
+         
          </form>
 
     </div>)
 }
 
-export default Search
