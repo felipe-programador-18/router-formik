664ec7b957521f0e6bcb5c35ce7b2eae580e7b14
Felipe <machadofelipe2016@outlook.com>
2022-07-06 17:28:25 -0300

    c45-adding some things inside home

--- testing/src/pages/Home.js
+++ testing/src/pages/Home.js
@@ -1,24 +1,36 @@
 import React from 'react'
 import styles from "./homemodule.css"
 import { useFecthing } from '../hoock/useFetching'
-import { Link } from 'react-router-dom'
+import { Link} from 'react-router-dom'
 
 const Home = () => {
-    
+   
+    const url = "http://localhost:3000/products"
     //remind did destrucment about fetching!!
-     const {data:item, loading,error } = useFecthing()
-     console.log('somethings here', item)   
+    const {data:item, loading,error } = useFecthing(url)
+    
+    
+    
+    console.log('somethings here', item)   
      
     return ( <div className={styles.container} >
       {loading && <p>Loading......</p>}
+      {error && <p> here!! {error}</p>}
       <h2>here is home my frinds</h2>
 
       <ul>
-        {item && item.map((items) => (
-            <li key={items.id} > {items.name}- R$ {items.price} </li>
+        {item && item.map((product) => (
+            <li key={product.id} > <p>{product.name} </p>
+             <br/>
+             <h1>R$ {product.price}
+             </h1>
+
+             {/* remind i need create route dynamic here!! */}
+             <Link to={`/products/${product.id}`} >More information about link!!</Link>
+             </li>
         
         ))}
-        <Link to={''} >More information about link!!</Link>
+
       </ul>
     
    </div> ) 
