381061a3e5dcaf80a7a6fa15d87fce3e1fcd2e91
Felipe <machadofelipe2016@outlook.com>
2022-07-06 14:12:40 -0300

    c38-rearrage code and removed some thing

--- testing/src/index.js
+++ testing/src/index.js
@@ -2,24 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import { Routes, Route } from 'react-router';
-import Testing from './Morek/testing';
 
-import NavNav from './component/Navbar';
 
 //everything that adding or created new router I have passed here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <NavNav/>
-     <Routes>
-      <Route path='/' element={<App/>} />
-     </Routes>
-    </BrowserRouter>
+      <App/>
   </React.StrictMode>
   
 );
