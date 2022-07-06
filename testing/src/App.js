5b4bf29ff60dc9e6232c49c4e82db6f21973663b
Felipe <machadofelipe2016@outlook.com>
2022-07-06 17:29:55 -0300

    c47-adding more router and rearrange all code

--- testing/src/App.js
+++ testing/src/App.js
@@ -6,6 +6,9 @@ import StructurForm from './pages/formhoock';
 import NavNav from './component/Navbar';
 import Search from './component/Search';
 import Home from './pages/Home';
+import InfoProducts from './pages/Infoproducts';
+import Notfound from './pages/Notfound';
+import Product from './pages/Product';
 
 
 
@@ -24,8 +27,16 @@ function App() {
          <Route path='/' element={ <Home/>} />
          <Route path='/formularioone' element={<PracticMore/>} /> 
          <Route path='/formulariotwo' element={<StructurForm/>} /> 
-       
+         {/* create router about search to simulation aobut api */ }
          <Route path='/search'  element={<Search/>} /> 
+         
+         {/*create infoproducts router dynamic */}
+         <Route path='/products/:id' element={<Product/> }   />
+         <Route path='/products/:id/info'  element={<InfoProducts/>}  />  
+
+         <Route path='*' element={<Notfound/>}  />            
+
+
 
         </Routes>
   
