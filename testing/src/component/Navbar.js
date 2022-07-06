62c61b7f516295f1f8b48488188db8b0247e85ce
Felipe <machadofelipe2016@outlook.com>
2022-07-06 10:06:05 -0300

    c31-removed words home

--- testing/src/component/Navbar.js
+++ testing/src/component/Navbar.js
@@ -4,10 +4,9 @@ import { Link } from 'react-router-dom'
 const NavNav = () => {
   return(<>
   <nav> 
-    <h1>Home here</h1>
-
     <Link to={'/'} >Home</Link>
     <Link to={'/testing'} >Testing</Link>
+    <Link to={''} >Formulario de user</Link>
   </nav>
   
   </>)
